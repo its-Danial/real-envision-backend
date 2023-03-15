2579e329355783618634ab743857ad10beeb5ad2
its-Danial <itsmalikdanial@gmail.com>
2023-03-15 21:02:12 +0800

    update path

--- app/ml/dis_bg_removal.py
+++ app/ml/dis_bg_removal.py
@@ -26,8 +26,7 @@ else:
 
 
 # Download official weights
-if not os.path.exists("app/ml/saved_models"):
-    os.mkdir("app/ml/saved_models")
+if not os.path.exists("app/ml/saved_models/isnet.pth"):
     MODEL_PATH_URL = "https://drive.google.com/uc?id=1nV57qKuy--d5u1yvkng9aXW1KS4sOpOi"
     gdown.download(MODEL_PATH_URL, "app/ml/saved_models/isnet.pth", use_cookies=False)
 
